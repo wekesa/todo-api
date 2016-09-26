a0eef0cc53384180f54642de8597e9b165c9cd1d
Victor Wekesa <viwekesa@gmail.com>
2016-09-26 12:39:24 +0300

    added new production DB setup

--- db.js
+++ db.js
@@ -2,6 +2,21 @@
  * Created by wekesa on 9/25/16.
  */
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === "production"){
+    sequelize = new Sequelize(process.env.DATABASE_URL,{
+    	dialect:"postgres"
+    });
+}else{
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/data/dev-todo-api.sqlite'
+});
+}
+
+
 var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',
     'storage': __dirname + '/data/dev-todo-api.sqlite'
